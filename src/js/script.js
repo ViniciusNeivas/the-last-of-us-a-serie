2b1a13257c44f16288a26feb83779a2487943148
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-03-21 17:18:13 -0300

    Adição da funcionalidade de cada imagem do carrossel ter um texto distinto

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,37 @@
 const carouselButtons = document.querySelectorAll(".button");
 const images = document.querySelectorAll(".image");
+const imagesText = [
+  " The Last of Us é uma série distópica da HBO baseada na franquia de jogos de videogame de mesmo nome criada por Neil Druckmann. O drama narra um futuro pandêmico que foi devastador para humanidade, deixando os seres humanos à beira da extinção", 
+"ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", 
+"jjjjj", 
+"hhhhhh", 
+"kkkkk",
+"llllll"
+]
 
 let selectedButton = document.querySelector(".selected");
 let activeImage = document.querySelector(".active");
 
+showBackgroundImage(0);
+
 carouselButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
     disableSelectedButton();
-
+    
     activateSelectedButton(button);
-
-    hideActiveImage();
-
+    
     showBackgroundImage(index);
+   
+    hideActiveImage();
+    
   });
 });
 
 function showBackgroundImage(index) {
   images[index].classList.add("active");
   activeImage = images[index];
+  const description = document.querySelector(".description");
+  description.textContent = imagesText[index];
 }
 
 function activateSelectedButton(button) {
