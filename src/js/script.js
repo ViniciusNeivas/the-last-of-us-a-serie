8f640111bd76e7b10bc3db84a8f3ec1286709ecf
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-03-29 15:43:21 -0300

    Index: Alteração da ordem das imagem 3 e 4. Script: Adição de 5 frases para as 5 imagens que precisavam de frases

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,16 @@
 const carouselButtons = document.querySelectorAll(".button");
 const images = document.querySelectorAll(".image");
 const imagesText = [
-  " The Last of Us é uma série distópica da HBO baseada na franquia de jogos de videogame de mesmo nome criada por Neil Druckmann. O drama narra um futuro pandêmico que foi devastador para humanidade, deixando os seres humanos à beira da extinção", 
-"ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", 
-"jjjjj", 
-"hhhhhh", 
-"kkkkk",
-"llllll"
-]
+  " The Last of Us é uma série distópica da HBO baseada na franquia de jogos de videogame de mesmo nome criada por Neil Druckmann. O drama narra um futuro pandêmico que foi devastador para humanidade, deixando os seres humanos à beira da extinção",
+  "A história segue Joel, um sobrevivente que é contratado para escoltar Ellie, uma adolescente imune à doença, por todo o país em busca de uma cura.",
+  "Além dos protagonistas Joel e Ellie, a série apresenta uma variedade de personagens complexos e interessantes. Entre eles estão Tess (a esquerda), a parceira de Joel em missões anteriores, Marlene, líder dos Vaga-Lumes, um grupo de resistência contra a opressão do governo, e Bill, um sobrevivente solitário que vive em uma cidade abandonada.",
+  "A série conta com um elenco talentoso e aclamado, incluindo Pedro Pascal (conhecido por seu papel em The Mandalorian) como Joel, Bella Ramsey (Game of Thrones) como Ellie, e Gabriel Luna (Agents of S.H.I.E.L.D) como Tommy, irmão de Joel.",
+  "A série de The Last of Us é produzida pela HBO e está sendo desenvolvida por Craig Mazin, criador de Chernobyl, e Neil Druckmann, criador da franquia de jogos. A dupla tem trabalhado de perto para garantir que a adaptação seja fiel à história e aos personagens originais.",
+  "A série de The Last of Us tem gerado muita expectativa entre fãs dos jogos e entusiastas de séries. A combinação do enredo envolvente, personagens cativantes e talentoso elenco sugere que a série tem tudo para ser um sucesso tanto entre os fãs como entre novos espectadores.",
+];
 
-let selectedButton = document.querySelector(".selected");
 let activeImage = document.querySelector(".active");
+let selectedButton = document.querySelector(".selected");
 
 showBackgroundImage(0);
 
